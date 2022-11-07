9154499c61e6348be1b07c07e8fc328ee1cba991
darsmen32@gmail.com <darsmen32@gmail.com>
2022-11-07 12:50:02 -0300

    Spring initializer HU2

---
+++ src/main/java/com/driques/HU2/Hu2Application.java
@@ -0,0 +1,13 @@
+package com.driques.HU2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hu2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hu2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/driques/HU2/Hu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driques.HU2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hu2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
